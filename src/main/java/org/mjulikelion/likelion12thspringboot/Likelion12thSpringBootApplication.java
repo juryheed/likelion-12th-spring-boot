4b4be0f7bdaedc2ce29b814dabcef8bd2e5a24b8
juryheed <rabitangl20@naver.com>
2024-04-13 16:33:19 +0900

    init spring boot

---
+++ src/main/java/org/mjulikelion/likelion12thspringboot/Likelion12thSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.likelion12thspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion12thSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Likelion12thSpringBootApplication.class, args);
+    }
+
+}
